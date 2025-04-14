784a544edc27371c8f9735c37f459d485f9dbdd0
Andrei Neustroev <andrei.neustroev@gcore.lu>
2025-04-14 18:03:12 +0300

    fix

--- schema/model_app_role_write_secret_id_response.go
+++ schema/model_app_role_write_secret_id_response.go
@@ -36,7 +36,7 @@ type AppRoleWriteSecretIdResponse struct {
 
 
 	// Duration in seconds after which the issued secret ID expires.
-	SecretIdTtl string `json:"secret_id_ttl,omitempty"`
+	SecretIdTtl int32 `json:"secret_id_ttl,omitempty"`
 
 }
 
